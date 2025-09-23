cc4b5d1faedd8b9a424bbaa0a7576aa5b3888a62
ngovankien-byte <ngokien.securitas@gmail.com>
2025-09-24 00:01:49 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+// Public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
